d6b3360b1bb7f111b51c6dbdb8a6ba4c342358ed
luoxq <xq.luo@foxmail.com>
2018-09-09 11:30:31 +0800

    Update ubuntu16.sh

--- ubuntu16.sh
+++ ubuntu16.sh
@@ -7,13 +7,16 @@ apt-get --assume-yes install python3-pip
 pip install shadowsocks
 
 IP=$( ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' )
+PWD=$(date +%y%m%d)
+
+echo Your PWD is: $(PWD)
 
 cat > /etc/shadowsocks.json <<EOF 
 {
    "server":"${IP}",
    "server_port":8443,
    "local_port":0,
-   "password":"shadow18",
+   "password":"Ss${PWD}",
    "timeout":600,
    "method":"aes-256-cfb"
 }
@@ -21,3 +24,5 @@ cat > /etc/shadowsocks.json <<EOF
 EOF
 
 ssserver -c /etc/shadowsocks.json -d start 
+sed -i '2issserver -c /etc/shadowsocks.json -d start' /etc/rc.local
+
